452febed0ce26fed672743e1027ef996127ade0f
Margo <romanova184@yandex.ru>
2025-09-16 13:14:00 +0300

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+// TODO: write code here
+
+// // comment this to pass build
+// const unusedVariable = "variable";
+
+// // for demonstration purpose only
+// export default function demo(value) {
+//   return `Demo: ${value}`;
+// }
+
+console.log("app.js included");
+let boardEl = document.createElement('div');
+boardEl.classList.add('board');
+let contEl = document.querySelector('.board-container');
+contEl.appendChild(boardEl);
+for (let index = 0; index < 16; index++) {
+  let cellEl = document.createElement('div');
+  cellEl.classList.add('A');
+  boardEl.appendChild(cellEl);
+}
+function createImg(number) {
+  let imgEl = document.createElement('img');
+  imgEl.classList.add('goblin');
+  let cellCollection = document.querySelectorAll('.A');
+  cellCollection[number].appendChild(imgEl);
+}
+
+function removeImg() {
+  let goblinEl = document.querySelector('.goblin');
+  if (goblinEl) {
+    goblinEl.remove();
+  }
+}
+
+function changePlace() {
+  removeImg();
+  let number = Math.floor(Math.random()*16);
+  createImg(number);
+}
+
+const IntervalId = setInterval(changePlace,1000);
+setTimeout(() => {clearInterval(IntervalId)}, 20000);  
