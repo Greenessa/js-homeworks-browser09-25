cf45bfabeb8521b15e6e05eb44a34039608ca348
Margo <romanova184@yandex.ru>
2025-09-19 15:42:37 +0300

    Next3 commit

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -1,15 +1,5 @@
 import changePlace from "./app";
-import {createImg} from "./app";
-
-// describe("Пример теста", () => {
-//   test.each([
-//     { str: "Hello!", expected: "Demo: Hello!" },
-//     { str: "", expected: "Demo: " },
-//     { str: 100, expected: "Demo: 100" },
-//   ])("demo($str)", ({ str, expected }) => {
-//     expect(demo(str)).toBe(expected);
-//   });
-// });
+import { createImg } from "./app";
 
 test('changePlace', () => {
   createImg = jest.fn();
